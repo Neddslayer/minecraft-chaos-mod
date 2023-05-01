f291b3187dfaafaeda237b68b3e857536d63d6b3
Neddslayer <neddslayer32@gmail.com>
2023-05-01 09:24:52 -0500

    we do a bit of logging

--- src/main/java/dev/neddslayer/chaosmod/ChaosMod.java
+++ src/main/java/dev/neddslayer/chaosmod/ChaosMod.java
@@ -1,10 +1,17 @@
 package dev.neddslayer.chaosmod;
 
 import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class ChaosMod implements ModInitializer {
+
+    public static final Logger LOGGER = LoggerFactory.getLogger("Chaos Mod");
+
     @Override
     public void onInitialize() {
+        LOGGER.info("The Chaos Orb has awaken...");
+
 
     }
 }
