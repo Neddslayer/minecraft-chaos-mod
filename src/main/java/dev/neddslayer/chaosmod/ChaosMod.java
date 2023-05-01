163c41779a7ac69c8f14db21008d47b32f8c463c
Neddslayer <neddslayer32@gmail.com>
2023-05-01 09:20:47 -0500

    change some more example mod stuff

---
+++ src/main/java/dev/neddslayer/chaosmod/ChaosMod.java
@@ -0,0 +1,10 @@
+package dev.neddslayer.chaosmod;
+
+import net.fabricmc.api.ModInitializer;
+
+public class ChaosMod implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
