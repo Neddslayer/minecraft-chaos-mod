560237fbe8a3740b2ae8d4b2f2eb512c8c4fee94
Neddslayer <neddslayer32@gmail.com>
2023-05-02 08:57:05 -0500

    uhhh :bangbang:

--- src/main/java/dev/neddslayer/chaosmod/mixin/BipedEntityModelMixin.java
+++ src/main/java/dev/neddslayer/chaosmod/mixin/BipedEntityModelMixin.java
@@ -2,20 +2,20 @@ package dev.neddslayer.chaosmod.mixin;
 
 import dev.neddslayer.chaosmod.registry.ItemRegistration;
 import net.minecraft.client.model.ModelPart;
+import net.minecraft.client.render.entity.model.AnimalModel;
 import net.minecraft.client.render.entity.model.CrossbowPosing;
+import net.minecraft.client.render.entity.model.ModelWithArms;
+import net.minecraft.client.render.entity.model.ModelWithHead;
 import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.Items;
+import net.minecraft.util.Arm;
 import net.minecraft.util.math.MathHelper;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
 
-import static dev.neddslayer.chaosmod.ChaosMod.CHAOS_LOGGER;
-
 @Mixin(net.minecraft.client.render.entity.model.BipedEntityModel.class)
-public abstract class BipedEntityModel<T extends LivingEntity> {
+public abstract class BipedEntityModelMixin<T extends LivingEntity> extends AnimalModel<T> implements ModelWithArms, ModelWithHead {
 
     @Shadow public net.minecraft.client.render.entity.model.BipedEntityModel.ArmPose leftArmPose;
 
@@ -27,6 +27,51 @@ public abstract class BipedEntityModel<T extends LivingEntity> {
 
     @Shadow @Final public ModelPart head;
 
+    @Shadow protected abstract Arm getPreferredArm(T entity);
+
+    @Shadow protected abstract ModelPart getArm(Arm arm);
+
+    @Shadow @Final public ModelPart body;
+
+    /**
+     * @author Neddslayer
+     * @reason because I need javadocs
+     */
+    @Overwrite
+    public void animateArms(T entity, float animationProgress) {
+        if (!(this.handSwingProgress <= 0.0F) && entity.getStackInHand(entity.getActiveHand()).getItem() != ItemRegistration.CHAOS_ORB) {
+            Arm arm = this.getPreferredArm(entity);
+            ModelPart modelPart = this.getArm(arm);
+            float f = this.handSwingProgress;
+            this.body.yaw = MathHelper.sin(MathHelper.sqrt(f) * 6.2831855F) * 0.2F;
+            ModelPart var10000;
+            if (arm == Arm.LEFT) {
+                var10000 = this.body;
+                var10000.yaw *= -1.0F;
+            }
+
+            this.rightArm.pivotZ = MathHelper.sin(this.body.yaw) * 5.0F;
+            this.rightArm.pivotX = -MathHelper.cos(this.body.yaw) * 5.0F;
+            this.leftArm.pivotZ = -MathHelper.sin(this.body.yaw) * 5.0F;
+            this.leftArm.pivotX = MathHelper.cos(this.body.yaw) * 5.0F;
+            var10000 = this.rightArm;
+            var10000.yaw += this.body.yaw;
+            var10000 = this.leftArm;
+            var10000.yaw += this.body.yaw;
+            var10000 = this.leftArm;
+            var10000.pitch += this.body.yaw;
+            f = 1.0F - this.handSwingProgress;
+            f *= f;
+            f *= f;
+            f = 1.0F - f;
+            float g = MathHelper.sin(f * 3.1415927F);
+            float h = MathHelper.sin(this.handSwingProgress * 3.1415927F) * -(this.head.pitch - 0.7F) * 0.75F;
+            modelPart.pitch -= g * 1.2F + h;
+            modelPart.yaw += this.body.yaw * 2.0F;
+            modelPart.roll += MathHelper.sin(this.handSwingProgress * 3.1415927F) * -0.4F;
+        }
+    }
+
     /**
      * @author Neddslayer
      * @reason because I need javadocs
@@ -34,10 +79,10 @@ public abstract class BipedEntityModel<T extends LivingEntity> {
     @Overwrite
     private void positionRightArm(T entity) {
         if (entity.getStackInHand(entity.getActiveHand()).getItem() == ItemRegistration.CHAOS_ORB) {
-            this.rightArm.yaw = -0.1F + (this.head.yaw / 4);
-            this.leftArm.yaw = 0.1F + (this.head.yaw / 4);
-            this.rightArm.pitch = -1.5707964F + this.head.pitch;
-            this.leftArm.pitch = -1.5707964F + this.head.pitch;
+            this.rightArm.yaw = -0.1F;
+            this.leftArm.yaw = 0.1F;
+            this.rightArm.pitch = -1.5707964F;
+            this.leftArm.pitch = -1.5707964F;
         }
         else
         {
@@ -83,10 +128,10 @@ public abstract class BipedEntityModel<T extends LivingEntity> {
     @Overwrite
     private void positionLeftArm(T entity) {
         if (entity.getStackInHand(entity.getActiveHand()).getItem() == ItemRegistration.CHAOS_ORB) {
-            this.rightArm.yaw = -0.1F + (this.head.yaw / 2);
-            this.leftArm.yaw = 0.1F + (this.head.yaw / 2);
-            this.rightArm.pitch = -1.5707964F + this.head.pitch;
-            this.leftArm.pitch = -1.5707964F + this.head.pitch;
+            this.rightArm.yaw = -0.1F;
+            this.leftArm.yaw = 0.1F;
+            this.rightArm.pitch = -1.5707964F;
+            this.leftArm.pitch = -1.5707964F;
         }
         else {
             switch (this.leftArmPose) {
---
+++ src/main/java/dev/neddslayer/chaosmod/mixin/HeldItemFeatureRendererMixin.java
@@ -0,0 +1,54 @@
+package dev.neddslayer.chaosmod.mixin;
+
+import dev.neddslayer.chaosmod.registry.ItemRegistration;
+import net.fabricmc.loader.impl.lib.sat4j.core.Vec;
+import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.render.entity.feature.FeatureRenderer;
+import net.minecraft.client.render.entity.feature.FeatureRendererContext;
+import net.minecraft.client.render.entity.feature.HeldItemFeatureRenderer;
+import net.minecraft.client.render.entity.model.EntityModel;
+import net.minecraft.client.render.entity.model.ModelWithArms;
+import net.minecraft.client.render.item.HeldItemRenderer;
+import net.minecraft.client.render.model.json.ModelTransformation;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.Arm;
+import net.minecraft.util.math.Vec3f;
+import org.spongepowered.asm.mixin.*;
+
+@Mixin(HeldItemFeatureRenderer.class)
+public abstract class HeldItemFeatureRendererMixin<T extends LivingEntity, M extends EntityModel<T> & ModelWithArms> extends FeatureRenderer<T, M> {
+    @Shadow @Final private HeldItemRenderer heldItemRenderer;
+
+    public HeldItemFeatureRendererMixin(FeatureRendererContext<T, M> context) {
+        super(context);
+    }
+
+    @Unique
+    private float animTime;
+
+    /**
+     * @author asdf
+     * @reason asdf
+     */
+    @Overwrite
+    public void renderItem(LivingEntity entity, ItemStack stack, ModelTransformation.Mode transformationMode, Arm arm, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light) {
+        if (!stack.isEmpty()) {
+            matrices.push();
+            animTime+= 0.5;
+            ((ModelWithArms)this.getContextModel()).setArmAngle(arm, matrices);
+            matrices.multiply(Vec3f.POSITIVE_X.getDegreesQuaternion(-90.0F));
+            matrices.multiply(Vec3f.POSITIVE_Y.getDegreesQuaternion(180.0F));
+            boolean bl = arm == Arm.LEFT;
+            matrices.translate((double)((float)(bl ? -1 : 1) / 16.0F), 0.125, -0.625);
+            if (stack.getItem() == ItemRegistration.CHAOS_ORB) {
+                matrices.multiply(Vec3f.POSITIVE_X.getDegreesQuaternion((float) (Math.toDegrees(Math.sin(animTime * 100)) * 150)));
+                matrices.multiply(Vec3f.POSITIVE_Y.getDegreesQuaternion((float) (Math.toDegrees(Math.sin(animTime * 100)) * 150)));
+                matrices.multiply(Vec3f.POSITIVE_Z.getDegreesQuaternion((float) (Math.toDegrees(Math.sin(animTime * 100)) * 150)));
+            }
+            this.heldItemRenderer.renderItem(entity, stack, transformationMode, bl, matrices, vertexConsumers, light);
+            matrices.pop();
+        }
+    }
+}
