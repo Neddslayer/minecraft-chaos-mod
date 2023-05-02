3affcd1ea707d7682db05f8fb1b4eadfb9287e8e
Neddslayer <neddslayer32@gmail.com>
2023-05-01 21:00:53 -0500

    i don't even know anymore

--- src/main/java/dev/neddslayer/chaosmod/ChaosMod.java
+++ src/main/java/dev/neddslayer/chaosmod/ChaosMod.java
@@ -4,6 +4,7 @@ import dev.neddslayer.chaosmod.registry.ItemRegistration;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.client.render.entity.model.BipedEntityModel;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -17,19 +18,5 @@ public class ChaosMod implements ModInitializer {
 
         ItemRegistration.registerItems();
 
-    }
-    @Environment(EnvType.CLIENT)
-    public static enum ArmPose {
-        CHAOS_ORB(false);
-
-        private final boolean twoHanded;
-
-        private ArmPose(boolean twoHanded) {
-            this.twoHanded = twoHanded;
-        }
-
-        public boolean isTwoHanded() {
-            return this.twoHanded;
-        }
     }
 }
---
+++ src/main/java/dev/neddslayer/chaosmod/mixin/BipedEntityModel.java
@@ -0,0 +1,114 @@
+package dev.neddslayer.chaosmod.mixin;
+
+import net.minecraft.client.model.ModelPart;
+import net.minecraft.client.render.entity.model.CrossbowPosing;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.util.math.MathHelper;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Shadow;
+
+@Mixin(net.minecraft.client.render.entity.model.BipedEntityModel.class)
+public abstract class BipedEntityModel<T extends LivingEntity> {
+
+    @Shadow public net.minecraft.client.render.entity.model.BipedEntityModel.ArmPose leftArmPose;
+
+    @Shadow @Final public ModelPart leftArm;
+
+    @Shadow public net.minecraft.client.render.entity.model.BipedEntityModel.ArmPose rightArmPose;
+
+    @Shadow @Final public ModelPart rightArm;
+
+    @Shadow @Final public ModelPart head;
+
+    /**
+     * @author Neddslayer
+     * @reason because I need javadocs
+     */
+    @Overwrite
+    private void positionRightArm(T entity) {
+        switch (this.rightArmPose) {
+            case EMPTY:
+                this.rightArm.yaw = 0.0F;
+                break;
+            case BLOCK:
+                this.rightArm.pitch = this.rightArm.pitch * 0.5F - 0.9424779F;
+                this.rightArm.yaw = -0.5235988F;
+                break;
+            case ITEM:
+                this.rightArm.pitch = this.rightArm.pitch * 0.5F - 0.31415927F;
+                this.rightArm.yaw = 0.0F;
+                break;
+            case THROW_SPEAR:
+                this.rightArm.pitch = this.rightArm.pitch * 0.5F - 3.1415927F;
+                this.rightArm.yaw = 0.0F;
+                break;
+            case BOW_AND_ARROW:
+                this.rightArm.yaw = -0.1F + this.head.yaw;
+                this.leftArm.yaw = 0.1F + this.head.yaw + 0.4F;
+                this.rightArm.pitch = -1.5707964F + this.head.pitch;
+                this.leftArm.pitch = -1.5707964F + this.head.pitch;
+                break;
+            case CROSSBOW_CHARGE:
+                CrossbowPosing.charge(this.rightArm, this.leftArm, entity, true);
+                break;
+            case CROSSBOW_HOLD:
+                CrossbowPosing.hold(this.rightArm, this.leftArm, this.head, true);
+                break;
+            case SPYGLASS:
+                this.rightArm.pitch = MathHelper.clamp(this.head.pitch - 1.9198622F - (entity.isInSneakingPose() ? 0.2617994F : 0.0F), -2.4F, 3.3F);
+                this.rightArm.yaw = this.head.yaw - 0.2617994F;
+                break;
+            case TOOT_HORN:
+                this.rightArm.pitch = MathHelper.clamp(this.head.pitch, -1.2F, 1.2F) - 1.4835298F;
+                this.rightArm.yaw = this.head.yaw - 0.5235988F;
+        }
+
+    }
+
+    /**
+     * @author Neddslayer
+     * @reason because I need javadocs
+     */
+    @Overwrite
+    private void positionLeftArm(T entity) {
+        switch (this.leftArmPose) {
+            case EMPTY:
+                this.leftArm.yaw = 0.0F;
+                break;
+            case BLOCK:
+                this.leftArm.pitch = this.leftArm.pitch * 0.5F - 0.9424779F;
+                this.leftArm.yaw = 0.5235988F;
+                break;
+            case ITEM:
+                this.leftArm.pitch = this.leftArm.pitch * 0.5F - 0.31415927F;
+                this.leftArm.yaw = 0.0F;
+                break;
+            case THROW_SPEAR:
+                this.leftArm.pitch = this.leftArm.pitch * 0.5F - 3.1415927F;
+                this.leftArm.yaw = 0.0F;
+                break;
+            case BOW_AND_ARROW:
+                this.rightArm.yaw = -0.1F + this.head.yaw - 0.4F;
+                this.leftArm.yaw = 0.1F + this.head.yaw;
+                this.rightArm.pitch = -1.5707964F + this.head.pitch;
+                this.leftArm.pitch = -1.5707964F + this.head.pitch;
+                break;
+            case CROSSBOW_CHARGE:
+                CrossbowPosing.charge(this.rightArm, this.leftArm, entity, false);
+                break;
+            case CROSSBOW_HOLD:
+                CrossbowPosing.hold(this.rightArm, this.leftArm, this.head, false);
+                break;
+            case SPYGLASS:
+                this.leftArm.pitch = MathHelper.clamp(this.head.pitch - 1.9198622F - (entity.isInSneakingPose() ? 0.2617994F : 0.0F), -2.4F, 3.3F);
+                this.leftArm.yaw = this.head.yaw + 0.2617994F;
+                break;
+            case TOOT_HORN:
+                this.leftArm.pitch = MathHelper.clamp(this.head.pitch, -1.2F, 1.2F) - 1.4835298F;
+                this.leftArm.yaw = this.head.yaw + 0.5235988F;
+        }
+
+    }
+}
