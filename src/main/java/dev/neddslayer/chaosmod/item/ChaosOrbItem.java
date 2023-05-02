3e1cdcc11209288589a258905560a378e20b9065
Neddslayer <neddslayer32@gmail.com>
2023-05-01 19:50:20 -0500

    stuff

--- src/main/java/dev/neddslayer/chaosmod/item/ChaosOrbItem.java
+++ src/main/java/dev/neddslayer/chaosmod/item/ChaosOrbItem.java
@@ -1,12 +1,17 @@
 package dev.neddslayer.chaosmod.item;
 
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.text.Text;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
+import net.minecraft.util.math.Box;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
+
+import java.util.List;
 
 public class ChaosOrbItem extends Item {
     public ChaosOrbItem(Settings settings) {
@@ -16,9 +21,17 @@ public class ChaosOrbItem extends Item {
     @Override
     public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
         if (!world.isClient) {
-            user.sendMessage(Text.of("used it"));
+            user.addVelocity(0, 10, 0);
+            user.velocityModified = true;
+            user.getItemCooldownManager().set(this, 60);
             user.getStackInHand(hand).damage(2, user, (player) -> {
                 player.sendMessage(Text.of("it broke"));
+                List<LivingEntity> entities = world.getNonSpectatingEntities(LivingEntity.class, Box.of(user.getPos(), 20, 20, 20));
+                for (LivingEntity entity:
+                     entities) {
+                    Vec3d lookDir = entity.getRotationVector();
+                    entity.addVelocity(lookDir.getX() * -5,5,lookDir.getZ() * -5);
+                }
             } );
         }
         return super.use(world, user, hand);
