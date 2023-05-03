fc9a9019c2f485b2e47eb75c37520a3eee1171a1
Neddslayer <neddslayer32@gmail.com>
2023-05-02 20:33:15 -0500

    make it a EVEN MORE "chaos-like"

--- src/main/java/dev/neddslayer/chaosmod/mixin/HeldItemFeatureRendererMixin.java
+++ src/main/java/dev/neddslayer/chaosmod/mixin/HeldItemFeatureRendererMixin.java
@@ -1,5 +1,6 @@
 package dev.neddslayer.chaosmod.mixin;
 
+import com.mojang.blaze3d.systems.RenderSystem;
 import dev.neddslayer.chaosmod.ChaosMod;
 import dev.neddslayer.chaosmod.registry.ItemRegistration;
 import net.fabricmc.loader.impl.lib.sat4j.core.Vec;
@@ -13,6 +14,7 @@ import net.minecraft.client.render.item.HeldItemRenderer;
 import net.minecraft.client.render.model.json.ModelTransformation;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.Arm;
 import net.minecraft.util.math.Vec3f;
@@ -46,14 +48,24 @@ public abstract class HeldItemFeatureRendererMixin<T extends LivingEntity, M ext
             matrices.multiply(Vec3f.POSITIVE_Y.getDegreesQuaternion(180.0F));
             boolean bl = arm == Arm.LEFT;
             matrices.translate((double)((float)(bl ? -1 : 1) / 16.0F), 0.125, -0.625);
-            if (stack.getItem() == ItemRegistration.CHAOS_ORB) {
-                matrices.multiply(Vec3f.POSITIVE_Z.getDegreesQuaternion(ChaosMod.randomFloat(-20f, 20f)));
-                matrices.multiply(Vec3f.POSITIVE_X.getDegreesQuaternion(ChaosMod.randomFloat(-20f, 20f)));
-                matrices.multiply(Vec3f.POSITIVE_Y.getDegreesQuaternion(ChaosMod.randomFloat(-20f, 20f)));
-                matrices.translate(0, (Math.sin(animTime * 25) / 4) + 0.1, -0.05);
-                matrices.translate(ChaosMod.randomFloat(-0.025f, 0.025f), ChaosMod.randomFloat(-0.025f, 0.025f), ChaosMod.randomFloat(-0.025f, 0.025f));
-            }
+            if (stack.getItem() == ItemRegistration.CHAOS_ORB) matrices.translate(0, (Math.sin(animTime * 25) / 4) + 0.1, -0.05);
+
             this.heldItemRenderer.renderItem(entity, stack, transformationMode, bl, matrices, vertexConsumers, light);
+            if (stack.getItem() == ItemRegistration.CHAOS_ORB) {
+                matrices.translate(ChaosMod.randomFloat(-0.025f, 0.025f), ChaosMod.randomFloat(0f, 0.1f), ChaosMod.randomFloat(-0.025f, 0.025f));
+                RenderSystem.enableBlend();
+                RenderSystem.defaultBlendFunc();
+                RenderSystem.setShaderColor(0.05f, 0.05f, 0.05f, 0.5f);
+                for (int zxc = 0; zxc < 2; zxc++) {
+                    matrices.multiply(Vec3f.POSITIVE_Z.getDegreesQuaternion(ChaosMod.randomFloat(-20f, 20f)));
+                    matrices.multiply(Vec3f.POSITIVE_X.getDegreesQuaternion(ChaosMod.randomFloat(-20f, 20f)));
+                    matrices.multiply(Vec3f.POSITIVE_Y.getDegreesQuaternion(ChaosMod.randomFloat(-20f, 20f)));
+                    this.heldItemRenderer.renderItem(entity, stack, transformationMode, bl, matrices, vertexConsumers, light);
+                }
+                RenderSystem.setShaderColor(1.0f, 1.0f, 1.0f, 1.0f);
+                RenderSystem.disableBlend();
+            }
+
             matrices.pop();
         }
     }
---
+++ src/main/java/dev/neddslayer/chaosmod/mixin/HeldItemRendererMixin.java
@@ -0,0 +1,86 @@
+package dev.neddslayer.chaosmod.mixin;
+
+import com.google.common.base.MoreObjects;
+import com.mojang.blaze3d.platform.GlStateManager;
+import com.mojang.blaze3d.systems.RenderSystem;
+import dev.neddslayer.chaosmod.ChaosMod;
+import dev.neddslayer.chaosmod.registry.ItemRegistration;
+import net.minecraft.client.network.AbstractClientPlayerEntity;
+import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.render.item.HeldItemRenderer;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.Hand;
+import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.math.Vec3f;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(HeldItemRenderer.class)
+public abstract class HeldItemRendererMixin {
+
+    @Shadow protected abstract void renderFirstPersonItem(AbstractClientPlayerEntity player, float tickDelta, float pitch, Hand hand, float swingProgress, ItemStack item, float equipProgress, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light);
+
+    @Shadow private ItemStack mainHand;
+
+    @Shadow private float equipProgressMainHand;
+
+    @Shadow private float prevEquipProgressMainHand;
+
+    @Shadow private float prevEquipProgressOffHand;
+
+    @Shadow private float equipProgressOffHand;
+
+    @Shadow private ItemStack offHand;
+
+    @Shadow
+    static HeldItemRenderer.HandRenderType getHandRenderType(ClientPlayerEntity player) {
+        return null;
+    }
+
+    @Inject(method = "renderItem(FLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider$Immediate;Lnet/minecraft/client/network/ClientPlayerEntity;I)V", at = @At("HEAD"))
+    public void renderItem(float tickDelta, MatrixStack matrices, VertexConsumerProvider.Immediate vertexConsumers, ClientPlayerEntity player, int light, CallbackInfo ci) {
+        if (mainHand.getItem() == ItemRegistration.CHAOS_ORB) {
+            for (int zxc = 0; zxc < 2; zxc++) {
+                float f = player.getHandSwingProgress(tickDelta);
+                Hand hand = (Hand) MoreObjects.firstNonNull(player.preferredHand, Hand.MAIN_HAND);
+                float g = MathHelper.lerp(tickDelta, player.prevPitch, player.getPitch());
+                HeldItemRenderer.HandRenderType handRenderType = getHandRenderType(player);
+                float h = MathHelper.lerp(tickDelta, player.lastRenderPitch, player.renderPitch);
+                float i = MathHelper.lerp(tickDelta, player.lastRenderYaw, player.renderYaw);
+                matrices.multiply(Vec3f.POSITIVE_X.getDegreesQuaternion((player.getPitch(tickDelta) - h) * 0.1F));
+                matrices.multiply(Vec3f.POSITIVE_Y.getDegreesQuaternion((player.getYaw(tickDelta) - i) * 0.1F));
+                float j;
+                float k;
+                if (handRenderType != null && handRenderType.renderMainHand) {
+                    j = hand == Hand.MAIN_HAND ? f : 0.0F;
+                    k = 1.0F - MathHelper.lerp(tickDelta, this.prevEquipProgressMainHand, this.equipProgressMainHand);
+                    RenderSystem.enableBlend();
+                    RenderSystem.setShaderColor(1.0f, 1.0f, 1.0f, 0.5f);
+                    matrices.multiply(Vec3f.POSITIVE_Z.getDegreesQuaternion(ChaosMod.randomFloat(-1f, 1f)));
+                    matrices.multiply(Vec3f.POSITIVE_X.getDegreesQuaternion(ChaosMod.randomFloat(-1f, 1f)));
+                    matrices.multiply(Vec3f.POSITIVE_Y.getDegreesQuaternion(ChaosMod.randomFloat(-1f, 1f)));
+                    this.renderFirstPersonItem(player, tickDelta, g, Hand.MAIN_HAND, j, this.mainHand, k, matrices, vertexConsumers, light);
+                    RenderSystem.setShaderColor(1.0f, 1.0f, 1.0f, 1.0f);
+                    RenderSystem.disableBlend();
+                }
+
+                if (handRenderType != null && handRenderType.renderOffHand) {
+                    j = hand == Hand.OFF_HAND ? f : 0.0F;
+                    k = 1.0F - MathHelper.lerp(tickDelta, this.prevEquipProgressOffHand, this.equipProgressOffHand);
+                    matrices.multiply(Vec3f.POSITIVE_Z.getDegreesQuaternion(ChaosMod.randomFloat(-10f, 10f)));
+                    matrices.multiply(Vec3f.POSITIVE_X.getDegreesQuaternion(ChaosMod.randomFloat(-10f, 10f)));
+                    matrices.multiply(Vec3f.POSITIVE_Y.getDegreesQuaternion(ChaosMod.randomFloat(-10f, 10f)));
+                    this.renderFirstPersonItem(player, tickDelta, g, Hand.OFF_HAND, j, this.offHand, k, matrices, vertexConsumers, light);
+                }
+
+                vertexConsumers.draw();
+            }
+
+        }
+    }
+}
