eb00c45ecf87cd0af01f8e95373dfbd37ee0caf9
Neddslayer <neddslayer32@gmail.com>
2023-05-04 21:13:05 -0500

    make the chaos prot effect apply to LivingEntity types, not just Players

--- src/main/java/dev/neddslayer/chaosmod/mixin/LivingEntityMixin.java
+++ src/main/java/dev/neddslayer/chaosmod/mixin/LivingEntityMixin.java
@@ -6,23 +6,26 @@ import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.damage.DamageSource;
-import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.effect.StatusEffect;
 import net.minecraft.predicate.entity.EntityPredicates;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.List;
 
-import static dev.neddslayer.chaosmod.ChaosMod.CHAOS_LOGGER;
+@Mixin(LivingEntity.class)
+public abstract class LivingEntityMixin extends Entity {
 
-@Mixin(PlayerEntity.class)
-public abstract class PlayerEntityMixin extends LivingEntity {
+    @Shadow public abstract boolean hasStatusEffect(StatusEffect effect);
 
-    protected PlayerEntityMixin(EntityType<? extends LivingEntity> entityType, World world) {
+    @Shadow public abstract boolean removeStatusEffect(StatusEffect type);
+
+    protected LivingEntityMixin(EntityType<? extends LivingEntity> entityType, World world) {
         super(entityType, world);
     }
 
@@ -33,10 +36,8 @@ public abstract class PlayerEntityMixin extends LivingEntity {
             this.removeStatusEffect(ChaosMod.CHAOS_PROTECTION);
             List<LivingEntity> entities = world.getEntitiesByClass(LivingEntity.class, getBoundingBox().expand(5), EntityPredicates.VALID_LIVING_ENTITY);
             for (LivingEntity entity : entities) {
-                if (entity != this) {
-                    entity.damage(DamageSource.FLY_INTO_WALL, 4);
-                    entity.takeKnockback(2, entity.getX(), entity.getZ());
-                }
+                entity.damage(DamageSource.FLY_INTO_WALL, 4);
+                entity.takeKnockback(2, entity.getX(), entity.getZ());
             }
             cir.setReturnValue(false);
         }
